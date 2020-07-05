fcdfd45bb718d1ff1493db3d6aed674dfbfaf7bb
lipeng <654488010@qq.com>
2020-07-05 09:48:16 +0800

    高亮完成

--- src/views/search/suggess.vue
+++ src/views/search/suggess.vue
@@ -1,18 +1,27 @@
 <template>
   <div>
     <!-- 联想内容 -->
-    <van-cell size="large" :key="i" v-for="(item, i) in suggesslist">
-      <i slot="title" class="iconfont icon-search">{{ item }}</i>
+    <van-cell
+      size="large"
+      icon="search"
+      :key="i"
+      v-for="(item, i) in suggesslist"
+    >
+      <span slot="title" v-html="hightlight(item)"> </span>
     </van-cell>
   </div>
 </template>
 
 <script>
 import { getsuggess } from '@/api/user.js'
+import { debounce } from 'lodash'
+
 export default {
   name: 'searchSuggess',
   data() {
-    return { suggesslist: [] }
+    return {
+      suggesslist: []
+    }
   },
   props: {
     searchvalue: {
@@ -25,18 +34,35 @@ export default {
   watch: {
     searchvalue: {
       //   当searchtext发生改变的时候会调用，handle函数名称是固定的
-      async handler(value) {
-        //   发送请求触发联想，赋值渲染
-        const { data } = await getsuggess(value)
-        console.log(data.data)
-        this.suggesslist = data.data.options
-      },
+
+      handler: debounce(function(value) {
+        this.loadsuggess(value)
+      }, 200),
+
       // 该回调将在侦听开始之后被调用
       immediate: true
+    }
+  },
+  methods: {
+    async loadsuggess(q) {
+      //   发送请求触发联想，赋值渲染
+      try {
+        const { data } = await getsuggess(q)
+        console.log(data.data)
+        this.suggesslist = data.data.options
+      } catch (err) {}
+    },
+    hightlight(item) {
+      const hightlightstr = `<span class='active'>${this.searchvalue}</span>`
+      const reg = new RegExp(this.searchvalue, 'gi')
+      return item.replace(reg, hightlightstr)
     }
   }
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+/deep/ .van-cell .active {
+  color: #23a9f2;
+}
 </style>
