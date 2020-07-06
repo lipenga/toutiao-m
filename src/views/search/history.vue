8e4156d04601ec13094b4b57fdb21618509d486a
lipeng <654488010@qq.com>
2020-07-06 10:01:51 +0800

    搜索ok

--- src/views/search/history.vue
+++ src/views/search/history.vue
@@ -2,29 +2,55 @@
   <div>
     <!-- 搜索历史 -->
     <van-cell title="搜索历史" size="large">
-      <i slot="default" class="iconfont icon-delete"><span>全部删除</span></i>
+      <div v-if="showDelete">
+        <span @click="alldel">全部删除</span> &nbsp;&nbsp;<span
+          @click="showDelete = false"
+          >完成</span
+        >
+      </div>
+      <van-icon v-else name="delete" @click="showDelete = true" />
     </van-cell>
     <!-- 历史内容 -->
-    <div>
-      <van-cell title="hellwo" size="large"
-        ><van-icon name="close" />
-      </van-cell>
-      <van-cell title="hellwo" size="large"
-        ><van-icon name="close" />
-      </van-cell>
-      <van-cell title="hellwo" size="large"
-        ><van-icon name="close" />
-      </van-cell>
-      <van-cell title="hellwo" size="large"
-        ><van-icon name="close" />
-      </van-cell>
-    </div>
+
+    <van-cell
+      :title="item"
+      v-for="(item, i) in searchhistory"
+      :key="i"
+      @click="addordelete(item, i)"
+      ><van-icon name="close" v-if="showDelete" />
+    </van-cell>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  name: 'SearchHistory',
+  props: {
+    searchhistory: {
+      type: Array,
+      required: true
+    }
+  },
+  data() {
+    return { showDelete: false }
+  },
+  methods: {
+    // 全部删除
+    alldel() {
+      this.searchhistory.splice(0, this.searchhistory.length)
+    },
+    // 删除单个历史
+    addordelete(item, i) {
+      if (this.showDelete === false) {
+        //  非删除状态
+        this.$emit('additem', item)
+      } else {
+        //  删除状态
+        this.searchhistory.splice(i, 1)
+      }
+    }
+  }
+}
 </script>
 
-<style>
-</style>
+<style></style>
--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -8,6 +8,7 @@
         @search="onSearch"
         @cancel="onCancel"
         background="#308ffe"
+        @focus="back"
       />
     </form>
     <result v-if="this.showResult" :searchvalue="searchText"></result>
@@ -17,7 +18,11 @@
       @search="onSearch"
     >
     </suggess>
-    <history v-else></history>
+    <history
+      v-else
+      :searchhistory="searchHistories"
+      @additem="onSearch"
+    ></history>
   </div>
 </template>
 
@@ -25,30 +30,39 @@
 import suggess from './suggess.vue'
 import result from './searchresult.vue'
 import history from './history.vue'
+import { setItem, getItem } from '@/utils/storage.js'
 
 export default {
   name: 'searchIndex',
   data() {
     return {
       searchText: '',
-      showResult: false
+      showResult: false,
+      searchHistories: getItem('SEARCH-HISTORY') || []
     }
   },
   components: { suggess, history, result },
   methods: {
+    back() {
+      this.showResult = false
+    },
     //   点击回车搜索
-    async onSearch(val) {
-      this.showResult = true
-      console.log(val)
+    onSearch(val) {
       this.searchText = val
-      // try {
-      //   const { data } = await getresults(val)
-      //   console.log(data)
-      //   this.resultsList = data.data.results.title
-      // } catch (err) {}
+      const i = this.searchHistories.indexOf(val)
+      if (i !== -1) {
+        this.searchHistories.splice(i, 1)
+      }
+      this.searchHistories.unshift(val)
+      this.showResult = true
     },
     onCancel() {
       this.$router.back()
+    }
+  },
+  watch: {
+    searchHistories(val) {
+      setItem('SEARCH-HISTORY', val)
     }
   }
 }
